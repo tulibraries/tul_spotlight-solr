d7fbc81d6754f6ca6e52a5cc1e1e159dfdba0d30
Steven Ng <steven@nomadicoder.com>
2021-07-29 14:27:01 -0600

    Undo debug code.
    - See if this fixes the inability to download
    ansible-playubook-solrcloud
    - Leave in text change to ensure we see see the changes

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
-
-RESP=$(docker exec solr1 curl http://solr1:8983/solr/tul_spotlight/admin/ping?wt=json)
-
 STATUS=$(docker exec solr1 curl http://solr1:8983/solr/tul_spotlight/admin/ping?wt=json | jq .status)
-if [ "$STATUS" != 0 ]; then
+if [ "$STATUS" != '"OK"' ]; then
   echo "Faling because status is not OK or 0"
   echo "status: $STATUS"
-  echo "response: $RESP"
   exit -1
 fi
