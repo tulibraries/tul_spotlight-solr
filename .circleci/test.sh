65d92d207d3d00cc0147567d8c9388ae6ddb767a
Steven Ng <steven@nomadicoder.com>
2021-07-29 12:55:33 -0600

    Force circle ci with different commit

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
+
 RESP=$(docker exec solr1 curl http://solr1:8983/solr/tul_spotlight/admin/ping?wt=json)
+
 STATUS=$(docker exec solr1 curl http://solr1:8983/solr/tul_spotlight/admin/ping?wt=json | jq .status)
 if [ "$STATUS" != 0 ]; then
-  echo "Faling because status is not OK"
+  echo "Faling because status is not OK or 0"
   echo "status: $STATUS"
   echo "response: $RESP"
   exit -1
