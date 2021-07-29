88eae88662135c6b58b437d098b4c4828bdaf404
Steven Ng <steven@nomadicoder.com>
2021-07-29 11:19:01 -0600

    Fix test failure
    - Add tag to solr collection
    - Change expected status to '0' not 'OK"
    
    - DBG -- Remove when done
    - DBG -- Test for literal 0 -- Unquoted

--- .circleci/setup_solr.sh
+++ .circleci/setup_solr.sh
@@ -3,7 +3,7 @@ set -e
 cd ..
 git clone --single-branch --branch main git@github.com:tulibraries/ansible-playbook-solrcloud.git
 mkdir -p ansible-playbook-solrcloud/data/tmp/collections
-cp -r ~/project ansible-playbook-solrcloud/data/tmp/collections/tul_spotlight
+cp -r ~/project ansible-playbook-solrcloud/data/tmp/collections/tul_spotlight-solr
 cd ansible-playbook-solrcloud
 make build
 STATUS=$(docker exec solr1 curl -s -o /dev/null -w "%{http_code}" http://solr1:8983/solr)
--- .circleci/test.sh
+++ .circleci/test.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
+RESP=$(docker exec solr1 curl http://solr1:8983/solr/tul_spotlight/admin/ping?wt=json)
 STATUS=$(docker exec solr1 curl http://solr1:8983/solr/tul_spotlight/admin/ping?wt=json | jq .status)
-if [ "$STATUS" != '"OK"' ]; then
+if [ "$STATUS" != 0 ]; then
   echo "Faling because status is not OK"
   echo "status: $STATUS"
+  echo "response: $RESP"
   exit -1
 fi
