3d6fbf48aa6cadf916855bc31d6ba58fb3d2377e
Steven Ng <steven@nomadicoder.com>
2021-07-30 14:12:31 -0600

    Remove debug code

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
-RESP=$(docker exec solr1 curl http://solr1:8983/solr/tul_spotlight/admin/ping?wt=json)
 STATUS=$(docker exec solr1 curl http://solr1:8983/solr/tul_spotlight/admin/ping?wt=json | jq .status)
 if [ "$STATUS" != '"OK"' ]; then
   echo "Failing because status is not OK"
   echo "status: $STATUS"
-  echo "status: $RESP"
   exit -1
 fi
